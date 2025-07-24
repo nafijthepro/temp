2c0bc923a453e1ff6de4990dc1ddf313abf54e53
alkama844 <malkama844@gmail.com>
2025-07-25 03:34:45 +0600

    Update baby-sim-goat.js

--- scripts/cmds/baby-sim-goat.js
+++ scripts/cmds/baby-sim-goat.js
@@ -5,7 +5,7 @@ const baseApiUrl = async () => {
 
 module.exports.config = {
     name: "bby",
-    aliases: ["baby", "bbe", "Sizuka", "sizuka"],
+    aliases: ["janu", "bbe", "kukur", "bou"],
     version: "6.9.0",
     author: "dipto",
     countDown: 0,
